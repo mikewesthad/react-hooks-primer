58afc6bfddd3bf97c328c60ac966410bd881b53d
Michael Hadley <michaelwesthadley@gmail.com>
2020-08-19 11:40:12 -0500

    Move use context to demo 05

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import Counter from "./01-counter-use-state-demo/";
 import LightSwitch from "./02-lightswitch-use-state-demo/";
 import FocusedInput from "./03-focus-use-ref-demo";
-import ThemedApp from "./04-theme-use-context-demo";
+import ThemedApp from "./05-theme-use-context-demo";
 
 function App() {
   return (
